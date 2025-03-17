ddc13c91ec57172a743be9b11761fbc1ecd33fc4
Jahangir Hossain <jahangir@combosoft.co.uk>
2025-03-17 13:38:24 +0600

    Add reducer to use action types with switch statement

---
+++ src/redux/reducers/itemReducer.ts
@@ -0,0 +1,59 @@
+import {
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEM_ERROR,
+  ITEM_LOADING,
+  SET_ITEM_LIST,
+  UPDATE_ITEM,
+} from "../actions/actionTypes";
+
+type Item = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+type InitialState = {
+  items: Item[];
+  loading: boolean;
+  error: null | string;
+};
+
+const initialState: InitialState = {
+  items: [],
+  loading: false,
+  error: null,
+};
+
+const itemReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case ITEM_LOADING:
+      return { ...state, loading: false, error: null };
+    case SET_ITEM_LIST:
+      return { ...state, loading: false, items: action.payload };
+    case ADD_ITEM:
+      return { ...state, items: [...state.items, action.payload] };
+    case UPDATE_ITEM:
+      return {
+        ...state,
+        items: state.items.map((item) =>
+          item.id === action.payload.id ? action.payload : item
+        ),
+      };
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload),
+      };
+    case ITEM_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default itemReducer;
